a055cfbd971aded79409e679b490bef9954af796
Agustina <agustinagauna@gmail.com>
2021-01-31 23:32:32 -0300

    modificaciones

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,7 @@
 /*
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
-function mostrar()
-{
-	alert("Bienvenido Division A");
+function mostrar() {
+	alert("Esto funciona de maravilla");
 }
 
