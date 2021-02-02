64613019b4831d6356be456ffd7c2a992a0066d6
Agustina <agustinagauna@gmail.com>
2021-02-02 20:34:28 -0300

    Hasta ejercicio 7 realizado

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,20 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	var unNumero;
+	var otroNumero;
+	var suma;
+
+	//Entradas
+	unNumero= txtIdNumeroUno.value;
+	otroNumero= txtIdNumeroDos.value;
+
+	//Conversiones (Parseo)
+	unNumero = parseInt(unNumero);
+	otroNumero = parseInt(otroNumero);
+
+	suma = unNumero + otroNumero;
+
+	alert("La suma es " + suma);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,80 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	var unNumero;
+	var otroNumero;
+	var suma;
+
+	//Entradas
+	unNumero= txtIdNumeroUno.value;
+	otroNumero= txtIdNumeroDos.value;
+
+	//Conversiones (Parseo)
+	unNumero = parseInt(unNumero);
+	otroNumero = parseInt(otroNumero);
+
+	suma = unNumero + otroNumero;
+
+	alert("La suma es " + suma);	
 }
 
 function restar()
 {
+	var unNumero;
+	var otroNumero;
+	var resta;
+
+	//Entradas
+	unNumero= txtIdNumeroUno.value;
+	otroNumero= txtIdNumeroDos.value;
+
+	//Conversiones (Parseo)
+	unNumero = parseInt(unNumero);
+	otroNumero = parseInt(otroNumero);
+
+	resta = unNumero - otroNumero;
+
+	alert("La resta es " + resta);	
 	
 }
 
 function multiplicar()
 { 
+	var unNumero;
+	var otroNumero;
+	var multiplicacion;
+
+	//Entradas
+	unNumero= txtIdNumeroUno.value;
+	otroNumero= txtIdNumeroDos.value;
+
+	//Conversiones (Parseo)
+	unNumero = parseInt(unNumero);
+	otroNumero = parseInt(otroNumero);
+
+	multiplicacion = unNumero * otroNumero;
+
+	alert("La multiplicacion es " + multiplicacion);	
 	
 }
 
 function dividir()
 {
+	var unNumero;
+	var otroNumero;
+	var division;
+
+	//Entradas
+	unNumero= txtIdNumeroUno.value;
+	otroNumero= txtIdNumeroDos.value;
+
+	//Conversiones (Parseo)
+	unNumero = parseFloat(unNumero);
+	otroNumero = parseFloat(otroNumero);
+
+	division = unNumero / otroNumero;
+
+	alert("La division es " + division);	
 	
 }
 
