a0948425d794388ca62b4828c46ea8f9de58f7cd
Agustina <agustinagauna@gmail.com>
2021-02-07 01:06:40 -0300

    ejercicio 8, 9 y 10 de entrada y salida hechos

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,19 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var sueldo;
+	var resultado;
+	//var aumento;
 
-	alert("ok");
+	sueldo = txtIdSueldo.value;
+	sueldo = parseInt(sueldo);
+
+	// aumento = sueldo * 10/100;
+	/* aumento = sueldo * 0.1;
+	resultado = sueldo + aumento;*/
+	// resultado = sueldo + (sueldo * 0.1);
+	resultado = sueldo * 1.1;
+
+	txtIdResultado.value = resultado;
+	
 }
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,18 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	var importe;
+	var resultado;
+	// var descuento;
+
+	importe = txtIdImporte.value;
+	importe = parseInt(importe);
+
+	// descuento = importe * 25/100;
+	/* descuento = importe * 0.25;
+	resultado = importe - descuento;*/
+	//resultado = importe - (importe * 0.25); 
+
+	resultado = importe * 0.75;
+	txtIdResultado.value = resultado;
 }
